47b08f125b16fbf2eea2c1fa4b36446182a714ff
Isabelle-1 <isabelle.lathlean@gmail.com>
2021-07-13 11:36:10 +1000

    created tests for loan account

---
+++ src/com/mybank/account/LoanAccountTest.java
@@ -0,0 +1,31 @@
+package com.mybank.account;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class LoanAccountTest {
+
+	LoanAccount account;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		this.account = new LoanAccount("test customer","test account name", 0.0, "test account number", 10000.0);
+	}
+
+	@Test
+	public void constructor__has_limit_parameter() {
+		LoanAccount account = new LoanAccount("test customer", "test account name", 0.0, "test account number", 10000.0);
+		double limit = account.getLimit();
+		assertEquals(10000.0, limit);
+	}
+	
+	@Test
+	public void setBalance__greater_than_zero__throws() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			account.setBalance(42.0);
+		});
+		
+	}
+}
